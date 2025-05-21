39b2e71da59a97d8dcd5575a2d8e21c0dd8b3ab6
elaine-mern-dev <elaine@elainees.dev>
2025-05-21 17:27:30 +0300

    refactor: remove unecessary spaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 // src/App.tsx
 import Header from "./shared/layout/Header";
 
-
 function App() {
   return (
-    <div className="m-16  text-white h-screen"> 
+    <div className="m-16  text-white h-screen">
       <Header />
     </div>
   );

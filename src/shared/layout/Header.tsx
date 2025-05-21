28b9946ab15ba818df630f57a03253e88b15a2fc
elaine-mern-dev <elaine@elainees.dev>
2025-05-21 17:50:20 +0300

    feat: add CTA Button component to Header

--- src/shared/layout/Header.tsx
+++ src/shared/layout/Header.tsx
@@ -1,9 +1,10 @@
+import Button from "../ui/Button";
 import Nav from "./Nav";
 
 const Header:React.FC = () => {
   return (
-    <header className="bg-white p-4 rounded-full flex items-center space-x-6">
-      <h1 className="text-4xl text-orange-500  mb-2 mr-36">
+    <header className="bg-white p-4 rounded-full flex items-center justify-between space-x-6">
+      <h1 className="text-4xl text-orange-500  mb-2">
         <span className="bg-orange-500 text-2xl text-white p-4 mr-2  rounded-full">
           EM
         </span>
@@ -11,6 +12,7 @@ const Header:React.FC = () => {
       </h1>
 
       <Nav />
+      <Button text={"Let's Talk"} />
     </header>
   );
 }   

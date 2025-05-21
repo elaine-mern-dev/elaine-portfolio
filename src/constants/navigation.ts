0f4d02df6d019f939278383ce9718c46396ab37b
elaine-mern-dev <elaine@elainees.dev>
2025-05-21 17:08:34 +0300

    feat: add navigation.ts to store navigation items array

---
+++ src/constants/navigation.ts
@@ -0,0 +1,9 @@
+// src/constants/navigation.ts
+
+export const navItems = [
+    { name: "Home", href: "#home" },
+    { name: "About", href: "#about" },
+    { name: "Projects", href: "#projects" },
+    { name: "Contacts", href: "#contacts" },
+  ];
+  

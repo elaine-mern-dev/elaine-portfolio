a032b60290fa0bd387e7cecd8107189a7fc605c8
elaine-mern-dev <elaine@elainees.dev>
2025-05-21 16:55:29 +0300

    chore: import and render Header component in App.tsx with basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
+// src/App.tsx
+import Header from "./shared/layout/Header";
+
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold text-blue-600 mb-4">Welcome to My Portfolio</h1>
-        <p className="text-lg text-gray-700">
-          Built with <span className="font-semibold">Vite + React + TypeScript</span>
-        </p>
-      </div>
+    <div className="m-16  text-white h-screen"> 
+      <Header />
     </div>
   );
 }
 
 export default App;
-
-
-

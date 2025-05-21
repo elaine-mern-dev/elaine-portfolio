c93b94fcf8f4270081df2391785ce40f8cf3ce5c
elaine-mern-dev <elaine@elainees.dev>
2025-05-21 17:25:13 +0300

    feat: add Nav component to Header and apply spacing with Tailwind classes

--- src/shared/layout/Header.tsx
+++ src/shared/layout/Header.tsx
@@ -1,9 +1,16 @@
+import Nav from "./Nav";
+
 const Header:React.FC = () => {
   return (
-    <header className="bg-white text-white p-4 rounded-full">
-      <h1 className="text-4xl text-orange-500  mb-2">
-        <span className="bg-orange-500 text-2xl text-white p-4 mr-2  rounded-full">EM</span>
-      Elaine</h1>
+    <header className="bg-white p-4 rounded-full flex items-center space-x-6">
+      <h1 className="text-4xl text-orange-500  mb-2 mr-36">
+        <span className="bg-orange-500 text-2xl text-white p-4 mr-2  rounded-full">
+          EM
+        </span>
+        Elaine
+      </h1>
+
+      <Nav />
     </header>
   );
 }   

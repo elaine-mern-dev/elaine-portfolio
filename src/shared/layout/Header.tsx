ba03245dbc386ebdd4710ec7d805f1526649c5ce
elaine-mern-dev <elaine@elainees.dev>
2025-05-21 16:53:28 +0300

    feat: add Header component with styled name badge and rounded layout

---
+++ src/shared/layout/Header.tsx
@@ -0,0 +1,11 @@
+const Header:React.FC = () => {
+  return (
+    <header className="bg-white text-white p-4 rounded-full">
+      <h1 className="text-4xl text-orange-500  mb-2">
+        <span className="bg-orange-500 text-2xl text-white p-4 mr-2  rounded-full">EM</span>
+      Elaine</h1>
+    </header>
+  );
+}   
+
+export default Header;

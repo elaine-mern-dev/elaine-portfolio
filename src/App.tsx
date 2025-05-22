d686b314f59c18c662559231164b4a73e84f1555
elaine-mern-dev <elaine@elainees.dev>
2025-05-22 10:56:00 +0300

    feat: add Hero and About sections to App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 // src/App.tsx
+import About from "./features/About/About";
+import Hero from "./features/Hero/Hero";
 import Header from "./shared/layout/Header";
 
 function App() {
   return (
     <div className="m-16  text-white h-screen">
       <Header />
+      <Hero />
+      <About />
     </div>
   );
 }

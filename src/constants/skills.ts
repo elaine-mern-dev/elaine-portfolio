6f1c408de0f93492d540ce7fd35fe27d0d3f3f11
elaine-mern-dev <elaine@elainees.dev>
2025-05-22 22:01:05 +0300

    chore: add GitHub icon to skills array

--- src/constants/skills.ts
+++ src/constants/skills.ts
@@ -1,13 +1,29 @@
-//src/constants/skills.ts
+import {
+  FaHtml5,
+  FaJs,
+  FaReact,
+  FaNodeJs,
+  FaGithub, 
+} from 'react-icons/fa';
+import {
+  SiTypescript,
+  SiExpress,
+  SiMongodb,
+  SiSwagger,
+  SiTailwindcss,
+  SiRedux,
+} from 'react-icons/si';
+
 export const skills = [
-    { name: 'HTML5', img: '/images/html5.png', size: 'w-14 h-14' },
-    { name: 'JavaScript', img: '/images/js.png', size: 'w-12 h-12' },
-    { name: 'ReactJS', img: '/images/react.png', size: 'w-14 h-14' },
-    { name: 'TypeScript', img: '/images/ts.png', size: 'w-12 h-12' },
-    { name: 'NodeJS', img: '/images/node.png', size: 'w-14 h-14' },
-    { name: 'Express', img: '/images/express.png', size: 'w-14 h-14' },
-    { name: 'MongoDB', img: '/images/mongo.png', size: 'w-14 h-14' },
-    { name: 'Swagger', img: '/images/swagger.jpg', size: 'w-14 h-12' },
-    { name: 'TailwindCSS', img: '/images/tailwind.png', size: 'w-14 h-14' },
-    { name: 'Redux Tool Kit', img: '/images/redux.png', size: 'w-14 h-14' },
-  ];
+  { name: 'HTML5', icon: FaHtml5, size: 'text-5xl text-orange-600' },
+  { name: 'JavaScript', icon: FaJs, size: 'text-4xl text-yellow-500' },
+  { name: 'ReactJS', icon: FaReact, size: 'text-5xl text-cyan-400' },
+  { name: 'TypeScript', icon: SiTypescript, size: 'text-4xl text-blue-500' },
+  { name: 'NodeJS', icon: FaNodeJs, size: 'text-5xl text-green-600' },
+  { name: 'Express', icon: SiExpress, size: 'text-5xl text-gray-700' },
+  { name: 'MongoDB', icon: SiMongodb, size: 'text-5xl text-green-500' },
+  { name: 'Swagger', icon: SiSwagger, size: 'text-5xl text-green-700' },
+  { name: 'TailwindCSS', icon: SiTailwindcss, size: 'text-5xl text-sky-400' },
+  { name: 'Redux Toolkit', icon: SiRedux, size: 'text-5xl text-purple-600' },
+  { name: 'GitHub', icon: FaGithub, size: 'text-5xl text-black' },
+];

54d2ee71b255da4eaa78189c9b2f0bba77c45b8a
elaine-mern-dev <elaine@elainees.dev>
2025-05-22 22:13:23 +0300

    feat: update Skills component to use React icons instead of images

--- src/features/Skills/Skills.tsx
+++ src/features/Skills/Skills.tsx
@@ -7,13 +7,13 @@ const Skills = () => {
       <p className="text-lg text-gray-700">Here are some of my skills:</p>
 
       <div className="flex flex-wrap justify-center gap-4 mt-8">
-        {skills.map(({ name, img, size }) => (
+        {skills.map(({ name, icon: Icon, size }) => (
           <div
             key={name}
             className="w-64 sm:w-1/4 border border-gray-800 rounded-lg p-4 flex flex-col items-center justify-center text-center"
           >
-            <img src={img} alt={`${name} Logo`} className={`${size} mb-2`} />
-            <h3>{name}</h3>
+            <Icon className={`${size} mb-2`} />
+            <h3 className="mt-1 text-base font-semibold">{name}</h3>
           </div>
         ))}
       </div>

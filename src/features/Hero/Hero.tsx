5149642bb638249bf6bfcbe1e9e631cc88e46957
elaine-mern-dev <elaine@elainees.dev>
2025-05-22 11:02:54 +0300

    feat(hero): implement Hero section with personal intro, image, and CTA buttons

---
+++ src/features/Hero/Hero.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Button from "../../shared/ui/Button";
+
+const Hero = () => {
+  return (
+    <section className="flex items-center justify-between p-4">
+      <div className="-mt-28 w-1/2">
+        <p className="mb-4 py-2 px-4 border border-orange-500 rounded-full text-white text-center font-bold w-20">
+          Hello
+        </p>
+        <h1 className="text-6xl">
+          I'm <span className="text-orange-500">Elaine Muhombe,</span>
+          <br />A MERN Developer
+        </h1>
+        <h4 className="mt-4 text-2xl">
+          Building Scalable Applications with Seamless Integration and
+          Innovative Solutions
+        </h4>
+
+        {/* CTA Buttons */}
+        <div className="mt-8 flex gap-4">
+          <Button text="Portfolio" />
+          <button className=" inline-block px-4 py-2 text-white font-bold hover:bg-orange-500 transition-colors duration-300 rounded-full">
+            Hire Me
+          </button>
+        </div>
+      </div>
+      <div className="w-1/2">
+        <img
+          src="/images/elaine-photo.png"
+          alt="Elaine Muhombe"
+          className="max-w-full h-auto"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;

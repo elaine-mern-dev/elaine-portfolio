5edbd10aaa111552769ca489380d71472fc7ccdb
elaine-mern-dev <elaine@elainees.dev>
2025-05-22 12:05:28 +0300

    feat: add reusable skills data to constants file

---
+++ src/constants/skills.ts
@@ -0,0 +1,13 @@
+//src/constants/skills.ts
+export const skills = [
+    { name: 'HTML5', img: '/images/html5.png', size: 'w-14 h-14' },
+    { name: 'JavaScript', img: '/images/js.png', size: 'w-12 h-12' },
+    { name: 'ReactJS', img: '/images/react.png', size: 'w-14 h-14' },
+    { name: 'TypeScript', img: '/images/ts.png', size: 'w-12 h-12' },
+    { name: 'NodeJS', img: '/images/node.png', size: 'w-14 h-14' },
+    { name: 'Express', img: '/images/express.png', size: 'w-14 h-14' },
+    { name: 'MongoDB', img: '/images/mongo.png', size: 'w-14 h-14' },
+    { name: 'Swagger', img: '/images/swagger.jpg', size: 'w-14 h-12' },
+    { name: 'TailwindCSS', img: '/images/tailwind.png', size: 'w-14 h-14' },
+    { name: 'Redux Tool Kit', img: '/images/redux.png', size: 'w-14 h-14' },
+  ];

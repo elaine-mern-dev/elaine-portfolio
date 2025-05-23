a55c50bfc2b5c5c7830952e982303c2b6df60f8e
elaine-mern-dev <elaine@elainees.dev>
2025-05-23 11:21:38 +0300

    feat: add barrel file at src/features/index.ts for centralized exports

---
+++ src/features/index.ts
@@ -0,0 +1,5 @@
+//src/features/index.ts
+export { default as About } from "./About/About";
+export { default as Hero } from "./Hero/Hero";
+export { default as Projects } from "./Projects/Projects";
+export { default as Skills } from "./Skills/Skills";

e8f0bcdfe356bbc627199237e5ed7ed96d9bae36
elaine-mern-dev <elaine@elainees.dev>
2025-05-23 11:46:17 +0300

    feat: add Portfolio component with introductory content

---
+++ src/pages/Portfolio.tsx
@@ -0,0 +1,11 @@
+   const Portfolio:React.FC= () => {
+    return(
+        <div>
+            <h1>Portfolio</h1>
+            <p>Welcome to my portfolio page!</p>
+            <p>Here you can find a collection of my projects and works.</p>
+            <p>Feel free to explore and reach out if you have any questions!</p>
+        </div>
+    )
+   }
+   export default Portfolio;

2b1ffbc3cb3c947bbab6d484b171cf7a23e3eadf
elaine-mern-dev <elaine@elainees.dev>
2025-05-24 10:26:52 +0300

    feat: add Contact page component

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,14 @@
+//src/pages/Contact.tsx
+const Contact:React.FC = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">                    
+            <h1 className="text-4xl font-bold mb-4">Contact Us</h1>
+            <p className="text-lg text-gray-700">
+                We would love to hear from you! Please reach out to us at:
+            </p>
+            </div>
+                        
+    )
+
+}
+export default Contact;

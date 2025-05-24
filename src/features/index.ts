44136fa3b7ffa761293fce7e531f90978725984c
elaine-mern-dev <elaine@elainees.dev>
2025-05-24 10:51:40 +0300

    refactor: remove project exports from src/features/index.ts

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,5 +1,4 @@
 //src/features/index.ts
 export { default as About } from "./About/About";
 export { default as Hero } from "./Hero/Hero";
-export { default as Projects } from "./Projects/Projects";
 export { default as Skills } from "./Skills/Skills";

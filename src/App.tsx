2f7204217ad4602e57681c98bfbccc98889ac9bd
elaine-mern-dev <elaine@elainees.dev>
2025-05-24 10:09:45 +0300

    feat: set up routing with react-router-dom and project-specific routes
    - Replaced static Home component with Routes
    - Added paths for Home, About, Portfolio, Services, Contact
    - Included routes for individual projects: Edusynx, AgriNext, MerExpress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 // src/App.tsx
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Home, About, Portfolio, Services, Contact } from "./pages";
+import { AgriNext,Edusynx, MerExpress } from "./features/Projects";
 
-import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="m-16  text-white h-screen">
-      <Home />
+    <div className="m-16 text-white h-screen">
+      <Routes>
+        {/* Redirect root URL to /home */}
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contacts" element={<Contact />} />
+        <Route path="/projects/edusynx" element={<Edusynx />} />
+        <Route path="/projects/agrinext" element={<AgriNext />} />
+        <Route path="/projects/merexpress" element={<MerExpress />} />
+      </Routes>
     </div>
   );
 }

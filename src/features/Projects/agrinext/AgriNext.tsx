5b92ccf5867997fa14d5fdb64815da59da4ae925
elaine-mern-dev <elaine@elainees.dev>
2025-05-24 14:30:55 +0300

    feat(AgriNext): update title and description to 'AgriNext Africa', add PreviousButton

--- src/features/Projects/agrinext/AgriNext.tsx
+++ src/features/Projects/agrinext/AgriNext.tsx
@@ -1,12 +1,14 @@
 //src/features/Projects/agrinext/AgriNext.tsx
 import Button from "../../../shared/ui/Button";
+import PreviousButton from "../../../shared/ui/PreviousButton";
 
 const AgriNext:React.FC=()=> {
     return (
         <div className="p-6 min-h-screen">
-        <h2 className="text-3xl font-bold mb-8 text-gray">Agrisol</h2>
+            <PreviousButton title="back to projects" />
+        <h2 className="text-3xl font-bold mb-8 text-gray">AgriNext Africa</h2>
         <p className="text-gray-700 mb-4">
-            Agrisol is a MERN stack platform aimed at transforming agricultural operations and connecting farmers with smart analytics.
+            AgriNext Africa is a MERN stack platform aimed at transforming agricultural operations and connecting farmers with smart analytics.
         </p>
         <p className="text-gray-700 mb-4">
             The platform provides farmers with tools to manage their operations efficiently, access real-time data, and make informed decisions.

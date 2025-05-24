aa6d9b8ba9b3ad05c61f82fecb0fdb4c9bdb9646
elaine-mern-dev <elaine@elainees.dev>
2025-05-24 12:07:25 +0300

    refactor: extract social media links into reusable SocialMedia component in Contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,17 +1,21 @@
-import { Nav } from "../shared/layout";
-
 //src/pages/Contact.tsx
-const Contact:React.FC = () => {
-    return (
-        <div className="flex flex-col items-center justify-center px-4 max-w-3xl mx-auto"> 
-        <Nav />                   
-            <h1 className="text-4xl font-bold my-8">Contact Me</h1>
-            <p className="text-lg text-gray-700">
-               I would love to hear from you! Please reach out to me at:
-            </p>
-            </div>
-                        
-    )
+import { Nav } from "../shared/layout";
+import SocialMedia from "../shared/social-media-icons/SocialMedia";
 
-}
+const Contact: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center px-4 max-w-3xl mx-auto">
+      <Nav />
+      <h1 className="text-4xl font-bold my-8">Contact Me</h1>
+      <p className="text-lg text-gray-700 mb-4">
+        I would love to hear from you!
+      </p>
+
+      <div className="text-lg text-gray-800 space-y-2 mb-8">
+       <SocialMedia />
+      </div>
+    </div>
+  );
+};
+
 export default Contact;

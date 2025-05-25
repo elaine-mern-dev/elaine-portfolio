716d1dc5b833297ee72ae523cc86307173439daf
Elaine Muhombe <emuhombe@gmail.com>
2025-05-25 20:10:30 +0300

    refactor: remove extra spacing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,17 @@
 //src/pages/Home.tsx
-import {  Hero, About, Skills, Projects } from "../features";
+import { Hero, About, Skills, Projects } from "../features";
 import { Header, Footer } from "../shared/layout";
 
-
-const Home = () => {    
-    return (
-      <div>
-        <Header />
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Footer />
-      </div>
-    );
-}
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Footer />
+    </div>
+  );
+};
 export default Home;

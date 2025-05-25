bc184f4ff6330e2adaae7d10d0b05f6e157d826f
Elaine Muhombe <emuhombe@gmail.com>
2025-05-25 20:12:48 +0300

    style: enhance Footer layout and responsiveness with improved flexbox and spacing

--- src/shared/layout/Footer.tsx
+++ src/shared/layout/Footer.tsx
@@ -1,27 +1,28 @@
-//src/shared/layout/Footer.tsx
+// src/shared/layout/Footer.tsx
 import { IoIosArrowForward } from "react-icons/io";
 import { navItems } from "../../constants/navigation";
 import SocialMedia from "../social-media-icons/SocialMedia";
 
-const Footer:React.FC = () => {
+const Footer: React.FC = () => {
   return (
-    <footer className="mt-16 flex justify-evenly p-6  text-gray-700">
-      <div className="flex flex-col max-w-xs">
-        <p className="mb-4  text-orange-700 text-4xl">
-          <span className="bg-orange-700 text-2xl text-white p-4 mr-2  rounded-full">
+    <footer className="mt-16 flex flex-col md:flex-row justify-between items-center md:items-start gap-8 md:gap-0 p-6 text-gray-700 bg-slate-100">
+      {/* Logo & Info */}
+      <div className="flex flex-col items-center md:items-start max-w-xs text-center md:text-left">
+        <p className="mb-4 text-orange-700 text-3xl md:text-4xl flex items-center">
+          <span className="bg-orange-700 text-xl md:text-2xl text-white p-3 md:p-4 mr-2 rounded-full">
             EM
           </span>
           Elaine
         </p>
-
-        <p className="text-sm mt-2">
+        <p className="text-sm">
           &copy; 2025 Elaine Muhombe. All rights reserved.
         </p>
       </div>
-      {/* Navigation Links */}
-      <div className="flex flex-col mb-4">
+
+      {/* Navigation */}
+      <div className="flex flex-col items-center md:items-start">
         <h2 className="font-semibold mb-2">Navigation</h2>
-        <ul className="space-y-2">
+        <ul className="space-y-2 text-sm">
           {navItems.map(({ name, href }) => (
             <li
               key={name}
@@ -35,8 +36,9 @@ const Footer:React.FC = () => {
           ))}
         </ul>
       </div>
-      <div className="flex flex-col">
-        {/* Connect with Me */}
+
+      {/* Social Media */}
+      <div className="flex flex-col items-center md:items-start">
         <SocialMedia />
       </div>
     </footer>

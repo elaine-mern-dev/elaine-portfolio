123b68f4da8aa2b03a95808d3d07f351dedeb987
Elaine Muhombe <emuhombe@gmail.com>
2025-05-25 23:06:20 +0300

    fix: add leading slashes to navItems hrefs for correct routing

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -2,8 +2,8 @@
 
 export const navItems = [
     { name: "Home", href: "/" },
-    { name: "About", href: "about" },
-    { name: "Portfolio", href: "portfolio" },
-    { name: "Contacts", href: "contacts" },
+    { name: "About", href: "/about" },
+    { name: "Portfolio", href: "/portfolio" },
+    { name: "Contacts", href: "/contacts" },
   ];
   

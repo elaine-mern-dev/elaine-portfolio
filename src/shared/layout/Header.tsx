5afaee38ec1a371988d740e99fc64bcff2d1451f
Elaine Muhombe <emuhombe@gmail.com>
2025-05-25 20:14:10 +0300

    style: improve Header layout with responsive flex-wrap and spacing enhancements

--- src/shared/layout/Header.tsx
+++ src/shared/layout/Header.tsx
@@ -1,26 +1,26 @@
-//src/shared/layout/Header.tsx
+// src/shared/layout/Header.tsx
 import Button from "../ui/Button";
 import Nav from "./Nav";
 
-const Header:React.FC = () => {
+const Header: React.FC = () => {
   return (
-    <header className="bg-white p-4 rounded-full flex items-center justify-between space-x-6">
-      <h1 className="text-4xl text-orange-500  mb-2">
-        <span className="bg-orange-500 text-2xl text-white p-4 mr-2  rounded-full">
+    <header className="bg-transparent md:bg-white p-4 rounded-full flex flex-wrap md:flex-nowrap items-center justify-between gap-4 md:gap-6 md:mb-6">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl text-orange-500 flex items-center">
+        <span className="bg-orange-500 text-base sm:text-lg md:text-xl text-white p-2 sm:p-3 md:p-4 mr-2 rounded-full">
           EM
         </span>
         Elaine
       </h1>
 
-      <Nav />
-      <Button
-        text={"Let's Talk"}
-        onClick={() =>
-          window.open("https://www.linkedin.com/", "_blank")
-        }
-      />
+      <div className="w-full  md:w-auto flex justify-between items-center gap-4 flex-wrap md:flex-nowrap">
+        <Nav />
+        <Button
+          text={"Let's Talk"}
+          onClick={() => window.open("https://www.linkedin.com/", "_blank")}
+        />
+      </div>
     </header>
   );
-}   
+};
 
 export default Header;

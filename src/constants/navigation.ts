13fe471649e27281af867164cc6a17cbedb1af81
Elaine Muhombe <emuhombe@gmail.com>
2025-05-25 23:00:18 +0300

    fix: update Home nav link href from 'home' to '/'

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,7 +1,7 @@
 // src/constants/navigation.ts
 
 export const navItems = [
-    { name: "Home", href: "home" },
+    { name: "Home", href: "/" },
     { name: "About", href: "about" },
     { name: "Portfolio", href: "portfolio" },
     { name: "Contacts", href: "contacts" },

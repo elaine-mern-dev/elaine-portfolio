3ecb97a48c1ba12cf928a072eed62ecca1209556
Elaine Muhombe <emuhombe@gmail.com>
2025-05-25 14:02:33 +0300

    refactor: add navigation and resume download to AboutMeCTA component

--- src/components/AboutMeCTA.tsx
+++ src/components/AboutMeCTA.tsx
@@ -1,14 +1,28 @@
+//src/components/AboutMeCTA.tsx
 import Button from "../shared/ui/Button"
 import { IoMdCloudDownload } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 const AboutMeCTA = () => {
+    const navigate = useNavigate();
+    const handleResumeDownload = () => {
+      window.open(
+        "https://github.com/elaine-mern-dev/elaine-mern-dev/blob/main/resume_Elaine_Muhombe.pdf",
+        "_blank"
+      );
+    };
     return (
       <div className="flex mt-4">
         <Button
           text={"Contact Me"}
-          className="mr-4 bg-orange-500 text-white hover:bg-orange-600 transition duration-300  <IoMdCloudDownload />ease-in-out"
+          className="mr-4 bg-orange-500 text-white hover:bg-orange-600 transition duration-300"
+          onClick={() => navigate("/contacts")}
         />
-        <button className="border border-orange-500 rounded-full py-3 px-4 flex items-center gap-2">
+
+        <button
+          className="border border-orange-500 rounded-full py-3 px-4 flex items-center gap-2"
+          onClick={handleResumeDownload}
+        >
           Download my resume{" "}
           <span>
             <IoMdCloudDownload />

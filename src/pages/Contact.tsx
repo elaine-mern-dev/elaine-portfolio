82ea7fc8f7733b9695f5eca5fa824f2da9c208f8
Elaine Muhombe <emuhombe@gmail.com>
2025-05-26 12:40:54 +0300

    refactor(Contact): add Footer and restructure layout for better spacing and responsiveness

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,20 +1,34 @@
 //src/pages/Contact.tsx
-import { Nav } from "../shared/layout";
+import { Footer, Nav } from "../shared/layout";
 import SocialMedia from "../shared/social-media-icons/SocialMedia";
 
 const Contact: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center px-4 max-w-3xl mx-auto">
-      <Nav />
-      <h1 className="text-4xl font-bold my-8">Contact Me</h1>
-      <p className="text-lg text-gray-700 mb-4">
-        I would love to hear from you!
-      </p>
+    <>
+      <div className="md:-mt-36 w-full flex flex-col max-w-3xl mx-auto min-h-screen p-2">
+        <div className=" flex flex-col items-center justify-center  w-full flex-grow">
+          <>
+            <div className="grid grid-cols-1 justify-items-start -mt-48 md:mt-0 md:justify-items-center w-full">
+              <Nav />
+            </div>
 
-      <div className="text-lg text-gray-800 space-y-2 mb-8">
-       <SocialMedia />
+            <h1 className="text-4xl font-bold my-8">Contact Me</h1>
+            <p className="text-lg text-gray-700 mb-4">
+              I would love to hear from you!
+            </p>
+
+            <div className="text-lg text-gray-800 space-y-2 mb-8">
+              <SocialMedia />
+            </div>
+          </>
+        </div>
       </div>
-    </div>
+
+      {/* Full-width Footer */}
+      <div className="w-full">
+        <Footer />
+      </div>
+    </>
   );
 };
 

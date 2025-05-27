62205f89f4b03bda3946af3219deea87af38e86e
Elaine Muhombe <emuhombe@gmail.com>
2025-05-27 19:29:46 +0300

    chore(features): add Header export to features index

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,4 +1,6 @@
 //src/features/index.ts
+
+export { default as Header } from "./Header/Header";
 export { default as About } from "./About/About";
 export { default as Hero } from "./Hero/Hero";
 export { default as Projects } from "./Projects/Projects";

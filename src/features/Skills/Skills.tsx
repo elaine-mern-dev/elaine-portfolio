50cceaf3cbf41bf0bfdda8c16fddc33ecb4181d2
Elaine Muhombe <emuhombe@gmail.com>
2025-05-27 19:28:01 +0300

    feat(Skills): replace h1 with Badge component, update styling and layout

--- src/features/Skills/Skills.tsx
+++ src/features/Skills/Skills.tsx
@@ -1,22 +1,19 @@
 //src/features/Skills/Skills.tsx
 import { skills } from "../../constants/skills";
+import Badge from "../../shared/ui/Badge";
 
-const Skills = () => {
+const Skills: React.FC = () => {
   return (
-    <div className="mt-16 flex flex-col items-center justify-center px-4">
-      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 text-center">
-        My Skills
-      </h1>
+    <div className="mt-8 flex flex-col items-center justify-center px-4">
+      {/* Use Badge for section title */}
+      <Badge text="My Skills" />
       <p className="text-sm sm:text-base md:text-lg text-gray-400 text-center">
         Here are some of my skills:
       </p>
 
       <div className="flex flex-wrap justify-center gap-4 mt-8 w-full">
         {skills.map(({ name, icon: Icon, size }) => (
-          <div
-            key={name}
-            className="w-full sm:w-1/2 md:w-1/4 border border-gray-800 rounded-lg p-4 flex flex-col items-center justify-center text-center bg-slate-900"
-          >
+          <div className="w-full sm:w-1/2 md:w-1/4 rounded-lg p-4 flex flex-col items-center justify-center text-center bg-customBlueGray text-navy">
             <Icon className={`text-3xl sm:text-4xl md:${size} mb-2`} />
             <h3 className="text-sm sm:text-base md:text-lg font-semibold">
               {name}
@@ -24,6 +21,8 @@ const Skills = () => {
           </div>
         ))}
       </div>
+      {/* Horizontal rule */}
+      <hr className="my-12 border-t border-muted w-3/4" />
     </div>
   );
 };

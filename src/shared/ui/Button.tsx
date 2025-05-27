ce2447872b29c397f8fa0b35ecd0e018b8502b26
Elaine Muhombe <emuhombe@gmail.com>
2025-05-27 19:50:51 +0300

    refactor(ui): update Button styles to match new design system and remove className prop

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -8,17 +8,17 @@ interface ButtonProps {
   className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, onClick, className }) => {
+const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className={`bg-orange-400 hover:bg-orange-500 text-white font-semibold 
-        text-sm sm:text-base md:text-lg 
-        py-2 px-4 sm:py-3 sm:px-6 
-        rounded-full transition-colors duration-300 ${className}`}
+      className="bg-primary border border-primary hover:bg-secondary text-navy font-semibold
+    text-sm sm:text-base md:text-lg
+    py-2 px-4 sm:py-3 sm:px-6
+    rounded-full transition-colors duration-300 w-36 sm:w-auto"
     >
       <span className="flex items-center gap-1 sm:gap-2">
-        {text} <GoArrowUpRight />
+        {text} <GoArrowUpRight className="text-slateblue" />
       </span>
     </button>
   );
